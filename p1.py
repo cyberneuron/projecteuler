6b8a92d76b326882f6ecbf698c4f85d5455b3ab7
CyberNeuron <zhengqingqing56@gmail.com>
2016-01-12 01:12:32 +0800

    Optimize: Multiples of 3 and 5

--- p1.py
+++ p1.py
@@ -1,4 +1,4 @@
-#Find the sum of all the multiples of 3 or 5 below 1000. 
+# Find the sum of all the multiples of 3 or 5 below 1000. 
 
 n = input("below n: ")
 s = 0
@@ -6,3 +6,13 @@ for i in range(n):
     if i%3==0 or i%5==0:
         s = s + i
 print s
+
+# Optimize
+
+target = 999
+
+def div(n):
+    p = target//n
+    return n*(p*(p+1))/2
+
+div(3) + div(5) - div(15)
